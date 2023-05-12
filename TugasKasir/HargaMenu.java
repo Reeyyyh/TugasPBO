4f4f2c8686c5dcd8957fd9cbaafff3386233b001
Mohamad Raihan <129250616+Reeyyyh@users.noreply.github.com>
2023-05-12 19:57:51 +0700

    Update HargaMenu.java

--- TugasKasir/HargaMenu.java
+++ TugasKasir/HargaMenu.java
@@ -1,1 +1,17 @@
+package TugasKasir;
 
+public class HargaMenu {
+
+    private int Harga;
+
+    public int TotalHarga(){
+        int total = 0;
+        total = total + Harga;
+        return total;
+    }
+
+    public void SetHarga(int harga) {
+        this.Harga += harga;
+    }
+
+}
