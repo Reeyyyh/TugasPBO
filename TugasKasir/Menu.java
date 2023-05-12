eb022ef51fa00cd136c240daa9d560a37fd6115d
Mohamad Raihan <129250616+Reeyyyh@users.noreply.github.com>
2023-05-12 19:57:07 +0700

    Create Menu.java

---
+++ TugasKasir/Menu.java
@@ -0,0 +1,12 @@
+package TugasKasir;
+
+public class Menu {
+    public void tampilan(){
+            System.out.println("============  KASIR  ============");
+            System.out.println("1.Cappucino || Rp.15000");
+            System.out.println("2.Expresso  || Rp.13000");
+            System.out.println("3.Macchiato || Rp.11000");
+            System.out.println("0. ---  Check out  --- ");
+            System.out.println("=================================");
+    }
+}
